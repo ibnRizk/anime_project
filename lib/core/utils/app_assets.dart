4e8b010e58b282bde50945bd76280972f9e8209e
Hussein Rizk <husseinrizk117@gmail.com>
2025-09-26 15:08:27 +0300

    add image to project

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,73 @@
+class Assets {
+  Assets._();
+
+  /// Assets for assetsImagesCard
+  /// assets/images/card.png
+  static const String assetsImagesCard =
+      "assets/images/card.png";
+
+  /// Assets for assetsImagesDownload1
+  /// assets/images/download1.png
+  static const String assetsImagesDownload1 =
+      "assets/images/download1.png";
+
+  /// Assets for assetsImagesDownload2
+  /// assets/images/download2.png
+  static const String assetsImagesDownload2 =
+      "assets/images/download2.png";
+
+  /// Assets for assetsImagesFiles
+  /// assets/images/files.png
+  static const String assetsImagesFiles =
+      "assets/images/files.png";
+
+  /// Assets for assetsImagesImage2
+  /// assets/images/image2.png
+  static const String assetsImagesImage2 =
+      "assets/images/image2.png";
+
+  /// Assets for assetsImagesImage3
+  /// assets/images/image3.png
+  static const String assetsImagesImage3 =
+      "assets/images/image3.png";
+
+  /// Assets for assetsImagesImagedetective
+  /// assets/images/imagedetective.png
+  static const String assetsImagesImagedetective =
+      "assets/images/imagedetective.png";
+
+  /// Assets for assetsImagesImagehunter
+  /// assets/images/imagehunter.png
+  static const String assetsImagesImagehunter =
+      "assets/images/imagehunter.png";
+
+  /// Assets for assetsImagesImages1
+  /// assets/images/images1.png
+  static const String assetsImagesImages1 =
+      "assets/images/images1.png";
+
+  /// Assets for assetsImagesImagesmeal
+  /// assets/images/imagesmeal.png
+  static const String assetsImagesImagesmeal =
+      "assets/images/imagesmeal.png";
+
+  /// Assets for assetsImagesNaruto
+  /// assets/images/Naruto.png
+  static const String assetsImagesNaruto =
+      "assets/images/Naruto.png";
+
+  /// Assets for assetsImagesRocketBoy1
+  /// assets/images/RocketBoy1.png
+  static const String assetsImagesRocketBoy1 =
+      "assets/images/RocketBoy1.png";
+
+  /// Assets for assetsImagesStar1
+  /// assets/images/Star1.png
+  static const String assetsImagesStar1 =
+      "assets/images/Star1.png";
+
+  /// Assets for assetsImagesStar2
+  /// assets/images/Star2.png
+  static const String assetsImagesStar2 =
+      "assets/images/Star2.png";
+}
